04e9a16632842c772342982e0d1fed03b56c2921
ctqing <ctqing123@outlook.com>
2026-08-05 15:22:37 +0800

    学习平台

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'learn-v6';
+const PRE_CACHE = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRE_CACHE).catch(() => {})));
+  self.skipWaiting();
+});
+
+// HTML 使用 Network First，其他资源 Cache First
+self.addEventListener('fetch', e => {
+  const isNav = e.request.mode === 'navigate';
+  if (isNav) {
+    e.respondWith(
+      fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return resp;
+      }))
+    );
+  }
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// 通知客户端有新版本可用
+self.addEventListener('message', e => {
+  if (e.data === 'skipWaiting') self.skipWaiting();
+});
